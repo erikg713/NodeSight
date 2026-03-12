d11ca70bd09ff97d6b0e0d07ceffa6ce8805b2ac
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-03-12 08:31:51 -0500

    Update NodeSightCluster.js

--- src/services/NodeSightCluster.js
+++ src/services/NodeSightCluster.js
@@ -1,1 +1,45 @@
+class NodeSightCluster {
+  constructor() {
+    this.socket = null
+    this.url = process.env.REACT_APP_NODE_WS_URL || "wss://node-cluster.pinetwork.com/ws"
+    this.reconnectAttempts = 0
+    this.maxReconnects = 5
+  }
 
+  connect(onMessage) {
+    this.socket = new WebSocket(this.url)
+
+    this.socket.onopen = () => {
+      console.log("[NodeSightCluster] Connected")
+      this.reconnectAttempts = 0
+    }
+
+    this.socket.onmessage = (event) => {
+      try {
+        onMessage(JSON.parse(event.data))
+      } catch {
+        console.warn("Invalid cluster message")
+      }
+    }
+
+    this.socket.onclose = () => {
+      console.warn("[NodeSightCluster] Disconnected")
+
+      if (this.reconnectAttempts < this.maxReconnects) {
+        this.reconnectAttempts++
+
+        setTimeout(() => {
+          this.connect(onMessage)
+        }, 2000 * this.reconnectAttempts)
+      }
+    }
+  }
+
+  sendUpdate(payload) {
+    if (this.socket?.readyState === WebSocket.OPEN) {
+      this.socket.send(JSON.stringify(payload))
+    }
+  }
+}
+
+export const nsCluster = new NodeSightCluster()
