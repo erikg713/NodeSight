cdbb036cbed72e25078c1802c39ef3cb62028a56
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-03-17 07:25:54 -0500

    Update ImageCompressor.js

--- src/services/ImageCompressor.js
+++ src/services/ImageCompressor.js
@@ -59,4 +59,34 @@ class ImageCompressor {
   }
 }
 
+/**
+ * ImageCompressor
+ * Resizes and compresses images to save bandwidth.
+ */
+class ImageCompressor {
+  async compress(dataUrl, { maxWidth = 640, quality = 0.7 } = {}) {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.src = dataUrl;
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        let width = img.width;
+        let height = img.height;
+
+        if (width > maxWidth) {
+          height = Math.round((height * maxWidth) / width);
+          width = maxWidth;
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, width, height);
+        
+        resolve(canvas.toDataURL('image/jpeg', quality));
+      };
+    });
+  }
+}
+
 export default new ImageCompressor();
